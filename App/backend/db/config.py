c7d5e7f60ec343269c0a9aac1dff1d29dcf33ccb
NguyenTien204 <vantiennguyen1424@gmail.com>
2025-07-16 17:31:55 +0700

    Update ORM

---
+++ App/backend/db/config.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from typing import Any
+
+POSTGRES_HOST = 'localhost'
+POSTGRES_PORT = 5432
+
+# Configuration for PostgreSQL in the movie data pipeline project
+POSTGRES_DB = 'movie_db'
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD = '141124'
+
+class PostgresConnection:
+    def __init__(self,
+                 db: str, user: str, pwd: str,
+                 host: str = "localhost", port: int = 5432):
+        self.url = f"postgresql://{user}:{pwd}@{host}:{port}/{db}"
+        self.engine = create_engine(self.url)
+
+    def dispose(self) -> None:
+        self.engine.dispose()
+
+    @staticmethod
+    def get_connection_string() -> str:
+        db = POSTGRES_DB
+        user = POSTGRES_USER
+        pwd = POSTGRES_PASSWORD
+        host = POSTGRES_HOST
+        port = POSTGRES_PORT
+        return f"postgresql://{user}:{pwd}@{host}:{port}/{db}"
