d8d28cdadc37069368e96d3526e4771ace49dd89
NguyenTien204 <vantiennguyen1424@gmail.com>
2025-07-29 11:06:53 +0700

    Update Kafka Consumer

---
+++ Data_Pipeline/kafka_consumer/kafka_consumer.py
@@ -0,0 +1,33 @@
+from kafka import KafkaConsumer
+import json
+import threading
+
+KAFKA_TOPICS = [
+    "userlog_click",
+    "userlog_rating",
+    "userlog_trailer",
+    "userlog_search",
+    "userlog_dwelltime"
+]
+
+def process_message(topic, message):
+    print(f"[{topic}] {message}")  # TODO: thay bằng lưu DB, Spark, gửi qua API, etc.
+
+def start_consumer():
+    consumer = KafkaConsumer(
+        *KAFKA_TOPICS,  # dấu * để unpack list
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='latest',
+        enable_auto_commit=True,
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+        group_id="pipeline-consumer-group"
+    )
+
+    print("[Kafka Consumer] Đang lắng nghe các topic:", KAFKA_TOPICS)
+
+    for msg in consumer:
+        process_message(msg.topic, msg.value)
+
+# Nếu muốn chạy riêng:
+if __name__ == "__main__":
+    start_consumer()
